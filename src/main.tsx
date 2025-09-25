69f0836d9111cdb8574eaa163a569e1d94f19f15
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2025-09-25 13:44:00 -0300

    alterando browseRouter para HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { GlobalStyle } from './style/globalStyle.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/challenger-pokeAPI-2.0">
+  <HashRouter >
     <StrictMode>
       <GlobalStyle />
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
