bd36ad1cdbf99e9c29fa47030f1930c6d1ae769e
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2025-09-25 13:45:16 -0300

    retornando para BrowseRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './style/globalStyle.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <HashRouter >
+  <BrowserRouter basename="/challenger-pokeAPI-2.0">
     <StrictMode>
       <GlobalStyle />
       <App />
     </StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 )
