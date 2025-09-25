0cf118655e5143c466150bd39ef7a28d5183cbb6
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2025-09-25 13:40:44 -0300

    adicionando browseRoutes ao main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './style/globalStyle.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter basename="/challenger-pokeAPI-2.0">
+    <StrictMode>
+      <GlobalStyle />
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 )
